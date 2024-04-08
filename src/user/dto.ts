93acf50ab0721c136814855badc903f791ebffc8
dc-21 <cholahkuboko@gmail..com>
2024-04-08 19:22:33 +0200

    some auth stuff

---
+++ src/user/dto.ts
@@ -0,0 +1,34 @@
+import { IsEmail, IsNotEmpty, IsString } from "class-validator";
+
+export class RegistorDto {
+  @IsNotEmpty()
+  @IsString()
+  readonly username: string;
+
+  @IsNotEmpty()
+  @IsEmail()
+  readonly email: string;
+
+  @IsNotEmpty()
+  @IsString()
+  readonly password: string;
+
+  constructor(d: RegistorDto) {
+    this.username = d.username;
+    this.email = d.email;
+    this.password = d.password;
+  }
+}
+
+export class LoginDto {
+  @IsNotEmpty()
+  readonly email: string;
+
+  @IsNotEmpty()
+  readonly password: string;
+
+  constructor(d: LoginDto) {
+    this.email = d.email;
+    this.password = d.password;
+  }
+}
